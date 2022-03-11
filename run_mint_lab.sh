954070c4f8caea0be228490584edc5a00f3aad3b
Felix Riedel <felix.riedel@make-it-mint.de>
2022-03-11 15:55:27 +0100

    updated to pyqt6

---
+++ run_mint_lab.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+#create venv if it does not exist
+test -d "$SCRIPT_DIR/.mint_venv" \
+    || { \
+        python3 -m venv "$SCRIPT_DIR/.mint_venv" \
+        && source "$SCRIPT_DIR/.mint_venv/bin/activate" \
+        && pip3 install -r "$SCRIPT_DIR/requirements.txt"; \
+    }
+
+#activate venv and checkif requirements are met [optional], if not, install required libraries
+source "$SCRIPT_DIR/.mint_venv/bin/activate" \
+    && pip3 install -r "$SCRIPT_DIR/requirements.txt" \
+    && python3 "$SCRIPT_DIR/main.py"
